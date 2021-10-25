cfb919f00ce78cbb27595a5d86d34c278f87ca7a
MysteryDAO <MysteryDAOofficial@gmail.com>
2021-10-24 22:39:38 -0400

    pls

--- gh-pages.js
+++ gh-pages.js
@@ -3,8 +3,8 @@ var ghpages = require('gh-pages');
 ghpages.publish(
     'public', // path to public directory
     {
-        branch: 'gh-pages',
-        repo: 'https://github.com/MysteryDAO/MysteryDAO-site.git', // Update to point to your repository  
+        branch: 'site',
+        repo: 'https://github.com/MysteryDAO/site.git', // Update to point to your repository  
         user: {
             name: 'MysteryDAO', // update to use your name
             email: 'mysterydaoofficial@gmail.com' // Update to use your email
